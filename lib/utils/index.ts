3dd318bebabd01a097da9ad4f7abae8bb8bcc5bf
olakumaps <olakumaps@gmail.com>
2025-11-11 23:08:02 +0100

    chore: update utils exports

--- lib/utils/index.ts
+++ lib/utils/index.ts
@@ -1,4 +1,9 @@
 export * from './format';
 export * from './date';
 export * from './calculations';
+export * from './validation';
+export * from './browser';
+export * from './animation';
+export * from './color';
+export * from './share';
 
