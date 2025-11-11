9cd5d65f309a78f55536d98eab3914730104d377
olakumaps <olakumaps@gmail.com>
2025-11-11 22:57:53 +0100

    chore: add cards index

---
+++ components/wrapped/cards/index.ts
@@ -0,0 +1,9 @@
+export { WelcomeCard } from './WelcomeCard';
+export { TransactionsCard } from './TransactionsCard';
+export { GasSpentCard } from './GasSpentCard';
+export { MostActiveChainCard } from './MostActiveChainCard';
+export { TopTokensCard } from './TopTokensCard';
+export { NFTCard } from './NFTCard';
+export { SummaryCard } from './SummaryCard';
+export { ShareCard } from './ShareCard';
+
