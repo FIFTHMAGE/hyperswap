bd3f5361e6f7bd7c78b54eca0e5fda47fcc7c2e5
olakumaps <olakumaps@gmail.com>
2025-11-11 23:06:28 +0100

    chore: update next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  images: {
+    domains: ['logos.covalenthq.com'],
+  },
 };
 
 export default nextConfig;
