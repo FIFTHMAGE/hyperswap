bae50c03da69ecdfca94d84589df4f4fed77e28a
olakumaps <olakumaps@gmail.com>
2025-11-11 23:07:51 +0100

    chore: update UI components export

--- components/ui/index.ts
+++ components/ui/index.ts
@@ -1,4 +1,8 @@
 export { Button } from './Button';
 export { Card } from './Card';
 export { LoadingSpinner } from './LoadingSpinner';
+export { Badge } from './Badge';
+export { Container } from './Container';
+export { Divider } from './Divider';
+export { Skeleton } from './Skeleton';
 
