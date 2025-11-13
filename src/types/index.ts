87767f76a614f2c617f1139d6744911840b31897
olakumaps <olakumaps@gmail.com>
2025-11-13 15:44:39 +0100

    Update type barrel exports with all created type modules

--- src/types/index.ts
+++ src/types/index.ts
@@ -7,11 +7,35 @@
  * @module types
  */
 
-// Common types will be exported here as they are created
-// export * from './common';
-// export * from './blockchain';
-// export * from './token';
+// Common utility types
+export * from './common';
 
-// Placeholder for future type exports
-export {};
+// Blockchain types
+export * from './blockchain';
+export * from './token';
+
+// Domain types
+export * from './swap';
+export * from './liquidity/pool';
+export * from './liquidity/position';
+export * from './portfolio/balance';
+export * from './portfolio/transaction';
+export * from './wrapped/stats';
+export * from './wrapped/card';
+
+// System types
+export * from './analytics/metrics';
+export * from './analytics/events';
+export * from './api/response';
+export * from './api/error';
+export * from './realtime/websocket';
+export * from './realtime/subscription';
+export * from './user/wallet';
+export * from './user/preferences';
+export * from './ui/component';
+
+// Utility types
+export * from './guards';
+export * from './validators';
+export * from './branded';
 
