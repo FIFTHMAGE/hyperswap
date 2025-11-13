bda685c8990744e40b00fa4e70f80d831132540b
olakumaps <olakumaps@gmail.com>
2025-11-13 01:22:54 +0100

    feat: export new enhanced story cards

--- components/wrapped/cards/index.ts
+++ components/wrapped/cards/index.ts
@@ -6,4 +6,12 @@ export { TopTokensCard } from './TopTokensCard';
 export { NFTCard } from './NFTCard';
 export { SummaryCard } from './SummaryCard';
 export { ShareCard } from './ShareCard';
+export { PortfolioValueCard } from './PortfolioValueCard';
+export { ActiveDaysHeatmapCard } from './ActiveDaysHeatmapCard';
+export { MultiChainDistributionCard } from './MultiChainDistributionCard';
+export { DeFiProtocolsShowcaseCard } from './DeFiProtocolsShowcaseCard';
+export { SwapActivityTimelineCard } from './SwapActivityTimelineCard';
+export { FirstTransactionAnniversaryCard } from './FirstTransactionAnniversaryCard';
+export { MilestoneAchievementsCard } from './MilestoneAchievementsCard';
+export { UserComparisonCard } from './UserComparisonCard';
 
