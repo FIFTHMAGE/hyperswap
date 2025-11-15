e8455b46d8e27972374c5f93130bfa88cce3fbbd
olakumaps <olakumaps@gmail.com>
2025-11-15 03:56:07 +0100

    Update main hooks index to use subdirectory exports

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,33 +1,14 @@
 /**
  * Hooks barrel export
  * @module hooks
+ * 
+ * Organized into:
+ * - core: General utility hooks
+ * - domain: Business logic hooks
+ * - features: Feature-specific hooks
  */
 
-// Core hooks
-export * from './core/useWallet';
-export * from './core/useChainId';
-export * from './core/useBalance';
-export * from './core/useTransaction';
-export * from './core/useGasPrice';
-export * from './core/useToast';
-export * from './core/useLocalStorage';
-export * from './core/useDebounce';
-export * from './core/useAsync';
-export * from './core/useInterval';
-export * from './core/useCopyToClipboard';
-export * from './core/useMediaQuery';
-export * from './core/usePrevious';
-export * from './core/useOnClickOutside';
-export * from './core/useWindowSize';
-
-// Domain hooks
-export * from './domain/useSwapQuote';
-export * from './domain/useTokenBalances';
-export * from './domain/usePortfolio';
-export * from './domain/useLiquidityPools';
-export * from './domain/useWrappedStats';
-export * from './domain/useTokenPrice';
-export * from './domain/useSwapHistory';
-export * from './domain/useTokenSearch';
-export * from './domain/useFavoriteTokens';
-export * from './domain/useTransactionHistory';
+// Export all hooks from subdirectories
+export * from './core';
+export * from './domain';
+export * from './features';
