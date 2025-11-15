166d92c08326edf54264898574c1019e720846d2
olakumaps <olakumaps@gmail.com>
2025-11-15 03:55:32 +0100

    Add index file for domain hooks

---
+++ src/hooks/domain/index.ts
@@ -0,0 +1,21 @@
+/**
+ * Domain hooks - Business logic and domain-specific hooks
+ */
+export { useApproval } from './useApproval';
+export { useFavoriteTokens } from './useFavoriteTokens';
+export { useLimitOrders } from './useLimitOrders';
+export { useLiquidityPools } from './useLiquidityPools';
+export { usePortfolio } from './usePortfolio';
+export { useSwapHistory } from './useSwapHistory';
+export { useSwapQuote } from './useSwapQuote';
+export { useSwapSettings } from './useSwapSettings';
+export { useTokenBalance } from './useTokenBalance';
+export { useTokenBalances } from './useTokenBalances';
+export { useTok enFavorites } from './useTokenFavorites';
+export { useTokenPrice } from './useTokenPrice';
+export { useTokenPrices } from './useTokenPrices';
+export { useTokenSearch } from './useTokenSearch';
+export { useTransactionHistory } from './useTransactionHistory';
+export { useWrappedData } from './useWrappedData';
+export { useWrappedStats } from './useWrappedStats';
+
