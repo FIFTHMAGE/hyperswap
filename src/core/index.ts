d8b166ddb1f12ed59232eae4cc631b903b3ff7eb
olakumaps <olakumaps@gmail.com>
2025-11-17 20:28:42 +0100

    Create barrel exports for core and infrastructure modules

---
+++ src/core/index.ts
@@ -0,0 +1,9 @@
+/**
+ * Core module barrel exports
+ * @module core
+ */
+
+export * from './config';
+export * from './constants';
+export * from './contexts';
+export * from './lib';
--- src/infrastructure/index.ts
+++ src/infrastructure/index.ts
@@ -1,1 +1,7 @@
-export {};
+/**
+ * Infrastructure layer barrel exports
+ * @module infrastructure
+ */
+
+export * from './api';
+export * from './middleware';
