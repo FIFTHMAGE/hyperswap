1d885a9d1da4aba6af7cd3a7d0d759ab8ef32af5
olakumaps <olakumaps@gmail.com>
2025-11-17 20:30:04 +0100

    Refactor and move button components to shared/components/ui

---
+++ src/shared/components/ui/Button/index.ts
@@ -0,0 +1,8 @@
+/**
+ * Button component exports
+ * @module shared/components/ui/Button
+ */
+
+export { default as Button } from './Button';
+export { default as IconButton } from './IconButton';
+export { default as LoadingButton } from './LoadingButton';
