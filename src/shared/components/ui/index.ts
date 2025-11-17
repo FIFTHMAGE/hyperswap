8a5b2072b320f53ed7399bc3980f0853f98b4864
olakumaps <olakumaps@gmail.com>
2025-11-17 20:38:30 +0100

    Create comprehensive UI components barrel export

---
+++ src/shared/components/ui/index.ts
@@ -0,0 +1,68 @@
+/**
+ * Shared UI components barrel export
+ * @module shared/components/ui
+ */
+
+// Button components
+export * from './Button';
+
+// Input components
+export * from './Input';
+
+// Modal components
+export * from './Modal';
+export { default as Dialog } from './Dialog';
+
+// Card components
+export { default as Card } from './Card/Card';
+export { default as CardHeader } from './Card/CardHeader';
+export { default as CardBody } from './Card/CardBody';
+
+// Loading components
+export { default as Loader } from './Loading/Loader';
+export { default as LoadingSpinner } from './Loading/LoadingSpinner';
+export { default as Skeleton } from './Loading/Skeleton';
+export { default as Spinner } from './Loading/Spinner';
+
+// Feedback components
+export { default as Alert } from './Alert/Alert';
+export { default as Badge } from './Badge/Badge';
+export { default as Toast } from './Toast/Toast';
+export { default as Tooltip } from './Tooltip/Tooltip';
+
+// Form components
+export { default as Checkbox } from './Checkbox';
+export { default as Radio } from './Radio';
+export { default as RadioGroup } from './RadioGroup';
+export { default as Switch } from './Switch';
+export { default as Textarea } from './Textarea';
+export { default as Select } from './Select';
+export { default as Dropdown } from './Dropdown';
+
+// Navigation components
+export { default as Pagination } from './Pagination';
+export { default as Tabs } from './Tabs';
+export { default as Breadcrumb } from './Breadcrumb';
+export { default as Menu } from './Menu';
+
+// Data display components
+export { default as Avatar } from './Avatar';
+export { default as Chip } from './Chip';
+export { default as Tag } from './Tag';
+export { default as Divider } from './Divider';
+export { default as Accordion } from './Accordion';
+
+// Progress components
+export { default as Progress } from './Progress';
+export { default as ProgressBar } from './ProgressBar';
+export { default as Stepper } from './Stepper';
+
+// Overlay components
+export { default as Popover } from './Popover';
+export { default as Portal } from './Portal';
+export { default as Overlay } from './Overlay';
+
+// Utility components
+export { default as ThemeToggle } from './ThemeToggle';
+export { default as LanguageSwitcher } from './LanguageSwitcher';
+export { default as InstallPromptBanner } from './InstallPromptBanner';
