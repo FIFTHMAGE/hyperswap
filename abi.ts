55576849a97a377deeb0f537e65ca80384cdf37a
FIFTHMAGE <FIFTHMAGE@users.noreply.github.com>
2025-11-26 00:00:44 +0100

    docs: record HyperSwap deployment on Base Sepolia and export ABI

--- abi.ts
+++ abi.ts
@@ -45,3 +45,24 @@ export const HyperSwapABI = [
 export const HyperSwapAddress = "[0;34m[INFO][0m Deploying HyperSwap to Base Sepolia...
 [0;34m[INFO][0m Sending deployment transaction (this may take 30-60 seconds)...
 [1;33m[WARNING][0m Deployment timed out after 120 seconds. Continuing...";
+
+// HyperSwap
+export const HyperSwapABI = [
+  {
+    "inputs": [],
+    "name": "owner",
+    "outputs": [{"name":"","type":"address"}],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [{"name": "newOwner", "type": "address"}],
+    "name": "transferOwnership",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  }
+];
+export const HyperSwapAddress = "[0;34m[INFO][0m Deploying HyperSwap to Base Sepolia...
+[0;34m[INFO][0m Sending deployment transaction (this may take 30-60 seconds)...
+[1;33m[WARNING][0m Deployment timed out after 120 seconds. Continuing...";
